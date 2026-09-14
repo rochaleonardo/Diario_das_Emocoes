9a8d79173ece3e578a3975d85e5955e25dba7a19
rochaleonardo <leo.oi@me.com>
2026-09-14 18:03:04 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='diario-emocoes-v3';
+const CACHE_NAME='diario-emocoes-v4';
 const APP_FILES=['./','./index.html','./manifest.webmanifest','./icons/heart-brain-favicon-v3.png','./icons/heart-brain-ios-v3.png','./icons/heart-brain-192-v3.png','./icons/heart-brain-512-v3.png','./icons/heart-brain-maskable-v3.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
@@ -7,4 +7,3 @@ self.addEventListener('fetch',event=>{
   event.respondWith(fetch(event.request).then(response=>{
     const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;
   }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
-});
